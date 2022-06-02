a9842f731550bc33b7ce99e1c3002904ec548ec6
Chris270796 <84269447+Chris270796@users.noreply.github.com>
2022-06-02 11:15:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
-function preload() {}
+function preload() {
+
+}
 
 function setup() {
     canvas = createCanvas(640, 480);
-    canvas.position(110, 250);
+    canvas.position(150, 250);
     video = createCapture(VIDEO);
     video.hide();
-    tint_color = "";
+
 }
 
 function draw() {
-    image(video, 0, 0, 640, 480);
-    tint(tint_color);
+    push();
+    translate(width, 0);
+    scale(-1, 1);
+    image(video, 230, 150, 220, 200);
+    pop();
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(50, 50, 80);
+
+    circle(50, 430, 80);
+
+    circle(590, 50, 80);
+
+    circle(590, 430, 80);
+
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(90, 40, 460, 20);
+
+    rect(90, 420, 460, 20);
+
+    rect(40, 90, 20, 300);
+
+    rect(580, 90, 20, 300);
 }
 
 function take_snapshot() {
-    save("Coding_Rules.png");
-}
-
-function filter_tint() {
-    tint_color = document.getElementById("color_name").value;
+    save('student_name.png');
 }
